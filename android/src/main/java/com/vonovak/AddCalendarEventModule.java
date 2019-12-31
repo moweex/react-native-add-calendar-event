06db590ad575f003e946bf5843e96e97af791840
dinaashraf <dina.ashraf@mowesolutions.com>
2019-12-31 16:27:21 +0200

    fix not supporting android x yet

--- android/src/main/java/com/vonovak/AddCalendarEventModule.java
+++ android/src/main/java/com/vonovak/AddCalendarEventModule.java
@@ -12,7 +12,7 @@ import android.provider.CalendarContract;
 import android.app.LoaderManager;
 import android.content.Loader;
 import android.util.Log;
-import androidx.annotation.Nullable;
+import android.support.annotation.Nullable;
 
 import com.facebook.react.bridge.*;
 
--- android/src/main/java/com/vonovak/Utils.java
+++ android/src/main/java/com/vonovak/Utils.java
@@ -5,7 +5,7 @@ import android.content.ContentUris;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.CalendarContract;
-import androidx.annotation.Nullable;
+import android.support.annotation.Nullable;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
